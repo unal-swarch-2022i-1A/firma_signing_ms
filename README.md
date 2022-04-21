e3fb01965ad28d5b68224d907dc1fdcd8cfb11b5
faroseroc <faroseroc@unal.edu.co>
2022-04-20 20:50:12 -0500

    Docker funtional

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # firma_signing_ms
 Digital signing microservice
+
+```bash
+docker-compose --project-name "firma" up --detach
+docker exec -it firma_signing_ms /bin/bash
+```
---
+++ src/index.php
@@ -0,0 +1,14 @@
+<h1>¡Hola</h1>
+<h4>mundo!</h4>
+<p>unal-swarch-2022i-1A</p>
+<!--?php 
+$host = 'mysql';
+$user = 'root';
+$pass = 'rootpassword';
+$conn = new mysqli($host, $user, $pass);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected to MySQL successfully!";
+?-->
